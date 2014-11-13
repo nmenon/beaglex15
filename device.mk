e96f2e99f0830999687155078f332eb1c85cfad2
Nishanth Menon <nm@ti.com>
2014-11-12 18:18:59 -0600

    use the dra7 platform
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- device.mk
+++ device.mk
@@ -133,8 +133,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	media.aac_51_output_enabled=true
 
+# Force Android build inherit
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.hardware=beaglex15board
+
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
-$(call inherit-product-if-exists, hardware/ti/omap4xxx/jacinto6.mk)
+$(call inherit-product-if-exists, hardware/ti/dra7xx/jacinto6.mk)
 $(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
 $(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ti-jacinto6-vendor.mk)
 $(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ducati-full_beaglex15.mk)
